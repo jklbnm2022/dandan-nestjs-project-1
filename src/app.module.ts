5da181eeaa812a577fd9ed2f4ad05b148374d9b5
seongho <jklbnm2002@naver.com>
2022-10-18 18:22:33 +0900

    수정: 몽구스

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,27 @@ import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import * as mongoose from 'mongoose';
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URL), CatsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `${process.env.MONGO_URL_PREFIX}${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}${process.env.MONGO_URL_SUFFIX}${process.env.MONGO_DATABASE}`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    ),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
+  private readonly isDev: boolean = process.env.MODE === 'dev';
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoggerMiddleware).forRoutes('*');
+    mongoose.set('debug', this.isDev);
   }
 }
--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,8 +1,11 @@
+import { Cat, CatSchema } from './cats.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CatsController } from './cats.controller';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }])],
   providers: [CatsService],
   controllers: [CatsController],
 })
