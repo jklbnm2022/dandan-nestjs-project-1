038860e23cdde375ec6dd1cbce949c3cbb85182a
seongho <jklbnm2002@naver.com>
2022-10-18 18:21:43 +0900

    추가: bcrypt, swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder, OpenAPIObject } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // validation
+  app.useGlobalPipes(new ValidationPipe());
+  // catch filter
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  // run server
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
