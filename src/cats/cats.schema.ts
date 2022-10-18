0e3bc7a10bde64feb279e50312e606c4a86a8e5e
seongho <jklbnm2002@naver.com>
2022-10-18 18:22:19 +0900

    추가: cat schema

---
+++ src/cats/cats.schema.ts
@@ -0,0 +1,49 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, SchemaOptions } from 'mongoose';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+const options: SchemaOptions = {
+  timestamps: true,
+};
+
+@Schema(options)
+export class Cat extends Document {
+  @Prop({
+    required: true,
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @Prop({
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @Prop({
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @Prop({
+    required: false,
+  })
+  imgUrl: string;
+
+  readonly readOnlyData: { id: string; email: string; name: string };
+}
+
+export const CatSchema = SchemaFactory.createForClass(Cat);
+
+// 가상의 필드
+CatSchema.virtual('readOnlyData').get(function (this: Cat) {
+  return {
+    id: this.id,
+    email: this.email,
+    name: this.name,
+  };
+});
