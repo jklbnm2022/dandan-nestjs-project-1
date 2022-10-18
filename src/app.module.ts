9a44f4d894d0f4fef09423db1931aa7f76e82398
seongho <jklbnm2002@naver.com>
2022-10-18 15:58:33 +0900

    추가: 몽고DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [CatsModule],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URL), CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
