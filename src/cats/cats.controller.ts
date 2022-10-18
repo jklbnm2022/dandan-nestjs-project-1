55393ab38d9dcf4680b7ad1c8e8adcdc81596b4b
seongho <jklbnm2002@naver.com>
2022-10-18 18:21:50 +0900

    추가: 기초 crud

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,3 +1,4 @@
+import { CatRequestDto } from './dto/cats.request.dto';
 import {
   Controller,
   Delete,
@@ -9,10 +10,12 @@ import {
   Param,
   ParseIntPipe,
   UseInterceptors,
+  Body,
 } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { PositiveIntPipe } from '../common/pipes/positiveInt.pipe';
 import { SuccessInterceptor } from 'src/common/interceptors/logging.interceptor';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('cats')
 @UseInterceptors(SuccessInterceptor)
@@ -20,19 +23,36 @@ export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
-  getAllCat() {
+  getCurrentCat() {
     throw new HttpException('api broken', 401);
     return 'all cat';
+  }
+
+  @ApiOperation({ summary: '회원가입' })
+  @Post()
+  async signUp(@Body() body: CatRequestDto) {
+    console.log(body);
+    return await this.catsService.signUp(body);
+  }
+
+  @Post('login')
+  logIn() {
+    return 'logIn cat';
+  }
+
+  @Post('logout')
+  logOut() {
+    return 'logout';
+  }
+
+  @Post('upload/cats')
+  uploadCatImg() {
+    return 'uploadImg';
   }
 
   @Get(':id')
   getCat(@Param('id', ParseIntPipe, PositiveIntPipe) id: number) {
     return { id };
-  }
-
-  @Post()
-  createCat() {
-    return;
   }
 
   @Put(':id')
