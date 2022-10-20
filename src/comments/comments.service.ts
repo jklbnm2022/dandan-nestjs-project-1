340c0893b1fea2ef927bacf04fd0d6b717b3d735
seongho <jklbnm2002@naver.com>
2022-10-20 17:01:11 +0900

    추가: repository 패턴 적용

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,10 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Type } from '@nestjs/common';
+import { Types } from 'mongoose';
+import { CatsRepository } from 'src/cats/cats.repository';
+import { CommentsRepository } from './comments.repository';
+import { CommentsCreateDto } from './dto/comments.create.dto';
 
 @Injectable()
 export class CommentsService {
-  constructor() {}
+  constructor(
+    private readonly commentsRepository: CommentsRepository,
+    private readonly catsRepository: CatsRepository,
+  ) {}
 
-  async getAllComments() {}
+  async getAllComments() {
+    return await this.commentsRepository.getAllComment();
+  }
 
-  async createComment(id: string, body: any) {}
+  async createComment(
+    id: Types.ObjectId,
+    { contents, author }: CommentsCreateDto,
+  ) {
+    const targetCat = await this.catsRepository.findCatByIdWithoutPassword(id);
+
+    const validatedAuthor =
+      await this.catsRepository.findCatByIdWithoutPassword(author);
+
+    return await this.commentsRepository.createComment(
+      validatedAuthor._id,
+      targetCat._id,
+      contents,
+    );
+  }
+
+  async plusLike(id: Types.ObjectId) {
+    return await this.commentsRepository.plusLike(id);
+  }
 }
