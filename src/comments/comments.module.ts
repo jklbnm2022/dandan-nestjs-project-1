93e05d90394ce101838b9cca835b9e92fe2757c6
seongho <jklbnm2002@naver.com>
2022-10-20 17:02:09 +0900

    추가: 몽구스 연결 및 CatModule import

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CatsModule } from 'src/cats/cats.module';
 import { CommentsController } from './comments.controller';
+import { CommentsRepository } from './comments.repository';
+import { Comments, CommentsSchema } from './comments.schema';
 import { CommentsService } from './comments.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Comments.name, schema: CommentsSchema },
+    ]),
+    CatsModule,
+  ],
+  providers: [CommentsService, CommentsRepository],
   controllers: [CommentsController],
-  providers: [CommentsService]
 })
 export class CommentsModule {}
