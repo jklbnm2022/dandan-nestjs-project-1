7cab09267f80ee7e176e272eb1632edad9445e72
seongho <jklbnm2002@naver.com>
2022-10-21 17:19:58 +0900

    추가: aws s3 service(업로드, url 조회, 제거)

---
+++ src/aws/aws.service.ts
@@ -0,0 +1,78 @@
+import * as path from 'path';
+import * as AWS from 'aws-sdk';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PromiseResult } from 'aws-sdk/lib/request';
+
+//* sharp: 이미지 변환하고 싶을 때 사용할 수 있는 (변환 후 업로드) 패키지
+
+@Injectable()
+export class AwsService {
+  private readonly awsS3: AWS.S3;
+  public readonly S3_BUCKET_NAME: string;
+
+  constructor(private readonly configService: ConfigService) {
+    this.awsS3 = new AWS.S3();
+    this.awsS3.config.update({
+      credentials: {
+        accessKeyId: this.configService.get('S3_PUBLIC_ACCESS_KEY'),
+        secretAccessKey: this.configService.get('S3_SECRET_ACCESS_KEY'),
+      },
+    });
+    this.S3_BUCKET_NAME = this.configService.get('S3_BUCKET_NAME');
+  }
+
+  async uploadFileToS3(
+    folder: string,
+    file: Express.Multer.File,
+  ): Promise<{
+    key: string;
+    s3Object: PromiseResult<AWS.S3.PutObjectOutput, AWS.AWSError>;
+    contentType: string;
+  }> {
+    try {
+      const key = `${folder}/${Date.now()}_${path.basename(
+        file.originalname,
+      )}`.replace(/ /g, '');
+
+      const s3Object = await this.awsS3
+        .putObject({
+          Bucket: this.S3_BUCKET_NAME,
+          Key: key,
+          Body: file.buffer,
+          ACL: 'public-read',
+          ContentType: file.mimetype,
+        })
+        .promise();
+
+      return { key, s3Object, contentType: file.mimetype };
+    } catch (error) {
+      console.warn(error);
+      throw new BadRequestException(`파일 업로드 실패: ${error}`);
+    }
+  }
+
+  async deleteS3Object(
+    key: string,
+    callback?: (err: AWS.AWSError, data: AWS.S3.DeleteObjectOutput) => void,
+  ): Promise<{ success: true }> {
+    try {
+      await this.awsS3
+        .deleteObject(
+          {
+            Bucket: this.S3_BUCKET_NAME,
+            Key: key,
+          },
+          callback,
+        )
+        .promise();
+      return { success: true };
+    } catch (error) {
+      throw new BadRequestException(`파일 삭제 실패: ${error}`);
+    }
+  }
+
+  public getAwsS3FileUrl(objectKey: string) {
+    return `https://${this.S3_BUCKET_NAME}.s3.amazonaws.com/${objectKey}`;
+  }
+}
