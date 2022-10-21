73e78dcd057d677c0f0c97d30648967a90c5cf73
seongho <jklbnm2002@naver.com>
2022-10-21 16:42:36 +0900

    추가: provider 에 AwsService 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import * as mongoose from 'mongoose';
 import { CommentsModule } from './comments/comments.module';
+import { AwsService } from './aws.service';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
@@ -23,7 +24,7 @@ import { CommentsModule } from './comments/comments.module';
     CommentsModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AwsService],
 })
 export class AppModule implements NestModule {
   private readonly isDev: boolean = process.env.MODE === 'dev';
